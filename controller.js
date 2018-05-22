01ec8ce4362870c185828a0a2e3257731764f719
cameron <johnson.cameron.m@gmail.com>
2018-05-22 08:49:23 -0400

    part 2

--- controller.js
+++ controller.js
@@ -24,6 +24,42 @@ function TodoController(){
       completed: true
     }
   ]
+
+  vm.addTask = (task) => {
+    vm.list.push({
+      task: task,
+      completed: false
+    });
+    vm.task = "";
+  };
+
+  vm.editTask = (index, thing) => {
+    vm.showForm = true;
+
+    vm.temptask = {
+      task: thing
+    };
+    vm.tempIndex = index;
+  };
+  
+  vm.updateTask = (index, thing) => {
+    vm.list.splice(index, 1, { 
+      task: thing,
+      completed: false
+    });
+    vm.tempTask = "";
+    vm.showForm = false;
+    console.log("shoulda worked");
+  };
+
+  vm.removeTask = (index) => {
+    vm.list.splice(index, 1);
+  };
+
+  vm.completeTask = (index, thing) => {
+    vm.list[index].completed = true;
+  };
+  
 }
 
 
